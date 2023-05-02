3dd2bd3bc72f4056c3665e79f5228fe4380e84b1
Cong Wang <cwang@renci.org>
2023-05-01 22:26:13 -0400

    more update

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -140,7 +140,7 @@ def start_consumer(thread_queue, db_instance):
                 db_instance.add_key_value_pair_to_db(str(MESSAGE_ID), msg)
                 logger.debug("Save to database complete.")
                 logger.debug("message ID:" + str(MESSAGE_ID))
-                value = db_instance.read_from_db(MESSAGE_ID)
+                value = db_instance.read_from_db(str(MESSAGE_ID))
                 logger.debug("got value from DB:")
                 logger.debug(value)
                 MESSAGE_ID += 1
